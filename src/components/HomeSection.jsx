e8ee8dc265ec3a2c8ec61bef5089ae88c569e866
Kristian Gabot <kristiangabot@yahoo.com>
2025-06-06 21:14:38 +0800

    Added scroll visualizer at the bottom

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,3 +1,5 @@
+import { ArrowDown } from "lucide-react"
+
 export const HomeSection = () => {
     return (
         <section
@@ -45,6 +47,11 @@ export const HomeSection = () => {
                     </a>
                 </div>
                 </div>
+            </div>
+            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 opacity-0 animate-fade-in-delay-5">
+                <div className="flex flex-col items-center animate-bounce">
+                <span>Scroll</span>
+                <ArrowDown className="h-5 w-5 text-primary" /> </div>
             </div>
         </section>
 
