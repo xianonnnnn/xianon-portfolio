64e354be48abf6287b5f6994d823c297e2036371
Kristian Gabot <kristiangabot@yahoo.com>
2025-06-06 12:45:37 +0800

    Create theme (dark/light mode) button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from '../components/ThemeToggle';
+
 export const Home = () => {
     return (
-        <div className="min-h-screen ">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme Toggle */}
+            <ThemeToggle />
 
             {/* Background Effects */}
 
