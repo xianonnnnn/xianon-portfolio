a1991d1e4e55c30b9bbb822055047e9fff427eb4
Kristian Gabot <kristiangabot@yahoo.com>
2025-06-06 12:34:20 +0800

    Initialize files needed and finished designing initial CSS features.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen ">
+            {/* Theme Toggle */}
+
+            {/* Background Effects */}
+
+            {/* Navbar */}
+
+            {/* Main Content */}
+
+
+            {/* Footer */}
+        </div>
+    );
+}
