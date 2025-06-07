f890d4657d5eda743a86fca74ef43b71390635cd
Kristian Gabot <kristiangabot@yahoo.com>
2025-06-08 03:08:59 +0800

    Delete light mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Ensure dark mode is set by default
+document.documentElement.classList.add('dark');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { ThemeToggle } from '../components/ThemeToggle';
 import { StarBackground } from '../components/StarBackground';
 import { Navbar } from '../components/Navbar';
 import { HomeSection } from '../components/HomeSection';
@@ -19,9 +18,6 @@ export const Home = () => {
 
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-            {/* Theme Toggle */}
-            <ThemeToggle />
-
             {/* Background Effects */}
             <StarBackground />
 
