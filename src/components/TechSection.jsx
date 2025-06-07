97520d1806227b9504750689dc180951a43214f8
Kristian Gabot <kristiangabot@yahoo.com>
2025-06-07 13:20:55 +0800

    Added tech section

---
+++ src/components/TechSection.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { cn } from '@/lib/utils'
+
+
+const skills = [
+    // Frontend
+    {name: "HTML/CSS", category: "frontend"},
+    {name: "JavaScript", category: "frontend"},
+    {name: "React", category: "frontend"},
+    {name: "TypeScript", category: "frontend"},
+    {name: "Tailwind CSS", category: "frontend"},
+    {name: "Next.js", category: "frontend"},
+
+    // Backend
+    {name: "PostgreSQL", category: "backend"},
+
+    // Tools
+    {name: "Git/Github", category: "tools"},
+    {name: "Figma", category: "tools"},
+    {name: "Microsoft Excel", category: "tools"},
+    {name: "Google SpreadSheets", category: "tools"},
+    {name: "VS Code", category: "tools"},
+]
+
+const categories = ["all", "frontend", "backend", "tools"]
+
+export const SkillsSection = () => {
+    const [activeCategory, setActiveCategory] = useState("all");
+
+    const filteredSkills = skills.filter(
+        (skill) => activeCategory === "all" || skill.category === activeCategory)
+
+    return (
+        <section id="tech" className="py-24 px-4 relative bg-secondary/30">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    <span className="text-primary"> Tech</span> Stacks
+                </h2>
+
+                <div className="flex flex-wrap justify-center gap-4 mb-12">
+                    {categories.map((category, key) => (
+                        <button 
+                            key={key} 
+                            onClick={() => setActiveCategory(category)}
+                            className={cn("px-5 py-1 rounded-full transition-colors duration-300 capitalize",
+                                activeCategory === category 
+                                ? "bg-primary text-background" 
+                                : "bg-secondary/70 text-foreground hover:text-primary bg-secondary"
+                            )}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-3 gap-6">
+                    {filteredSkills.map((skill, key) => (
+                        <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover">
+                            <div className="text-left mb-4">
+                                <h3 className="font-semibold text-lg"> {skill.name}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
+        </section>)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { StarBackground } from '../components/StarBackground';
 import { Navbar } from '../components/Navbar';
 import { HomeSection } from '../components/HomeSection';
 import { AboutSection } from '../components/AboutSection';
+import { SkillsSection } from '../components/TechSection';
+
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -19,6 +21,7 @@ export const Home = () => {
             <main>
                 <HomeSection />
                 <AboutSection />
+                <SkillsSection />
             </main>
 
             {/* Footer */}
